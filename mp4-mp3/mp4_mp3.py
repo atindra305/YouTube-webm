ae6d9cecb9979e67103f342a18acbe55edf4a69c
Atindra Shekhar <56440918+atindra305@users.noreply.github.com>
2020-11-29 10:45:49 +0530

    Update mp4_mp3.py

--- mp4-mp3/mp4_mp3.py
+++ mp4-mp3/mp4_mp3.py
@@ -14,7 +14,7 @@ If the libraries are not installed just run the following command in your termin
  
 How to use the script:
 Just run the following command within your terminal replacing "NAME_OF_THE_VIDEO.mp4" by the name of your video file:
-$ python video_to_mp3.py NAME_OF_THE_VIDEO.mp4
+$ python mp4_mp3.py NAME_OF_THE_VIDEO.mp4
  
 Note.- The video must be within the same directory of the python script, otherwise provide the full path.
 """
